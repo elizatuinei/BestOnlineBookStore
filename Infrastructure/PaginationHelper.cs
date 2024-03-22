7e957334add8461b3d5d8f2ffda870eafe6e7d8d
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-03-22 16:20:50 -0600

    finalbookstore

--- Infrastructure/PaginationHelper.cs
+++ Infrastructure/PaginationHelper.cs
@@ -24,6 +24,13 @@ namespace BestOnlineBookStore.Infrastructure
 
         public PaginationInfo PageModel { get; set; }
 
+        public bool PageClassesEnabled { get; set; }
+
+        public string PageClass { get; set; } = String.Empty;
+
+        public string PageClassNormal { get; set; } = String.Empty;
+
+        public string PageClassSelected { get; set; } = String.Empty;
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null) 
@@ -37,6 +44,12 @@ namespace BestOnlineBookStore.Infrastructure
                     TagBuilder tag = new TagBuilder("a");
 
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new {pageNum  = i});
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
                     tag.InnerHtml.Append(i.ToString());
 
                     result.InnerHtml.AppendHtml(tag);
--- Program.cs
+++ Program.cs
@@ -31,7 +31,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+"pagination", "Books/{pageNum}", new {Controller = "Home", action="Index"});
 
 app.Run();
